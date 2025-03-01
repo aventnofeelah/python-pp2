99d58025d2a46bc0154a6563d63cb829b9551af3
= <=>
2025-03-01 10:40:14 +0500

    edited lab5

--- Lab5/ex-regex.py
+++ Lab5/ex-regex.py
@@ -1,6 +1,6 @@
 import re
 
-with open('C:\Code\python-files\python-univ\Lab5\products.txt', "r", encoding='utf-8') as file:
+with open('Lab5/products.txt', "r", encoding='utf-8') as file:
     lines = file.read().splitlines()
 
 #1
@@ -13,23 +13,23 @@ for x in lines:
     if re.findall("ab{2,3}", x):
         print(x)
 
-#3
+# #3
 for x in lines:
-    if re.findall("[a-z]+_[a-z]+", x):
+    if re.findall("[a-z]_[a-z]", x):
         print(x)
 
-#4
+# #4
 for x in lines:
-    if re.findall("[A-Z]+[a-z]+", x):
+    if re.findall("[A-Z][a-z]", x):
         print(x)
 
-#5
+# #5
 for x in lines:
-    if re.findall("^a.b$", x):
+    if re.findall("a.b$", x):
         print(x)
 
-#6
-with open('C:/Code/python-files/python-univ/Lab5/new-products.txt', "w", encoding='utf-8') as new_file:
+# #6
+with open('Lab5/products.txt', "w", encoding='utf-8') as new_file:
     for x in lines:
         temp = ""
         for j in x:
@@ -37,7 +37,7 @@ with open('C:/Code/python-files/python-univ/Lab5/new-products.txt', "w", encodin
                 temp += j
         new_file.write(temp + "\n")
             
-#7
+# #7
 def replaceFunc(match):
     print(match.group(0))
     return match.group(1).upper()
@@ -45,7 +45,7 @@ snake = input("Enter string in snake case: ")
 camel = re.sub("_([a-z])", replaceFunc, snake)
 print(camel)
 
-#8
+# #8
 def replaceFunc(match):
     print(match.group())
     return match.group(1) + " " + match.group(2)
@@ -53,7 +53,7 @@ s = "HelloBraveNewWorld"
 new_s = re.sub("([a-z])([A-Z])", replaceFunc, s)
 print(new_s)
 
-#10
+# #10
 def replaceFunc(match):
     return match.group(1) + "_" + match.group(2).lower()
 camel = input("Enter string in camel case: ")
